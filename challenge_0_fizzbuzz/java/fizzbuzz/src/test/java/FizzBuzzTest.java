83e0699a6ae4eb265b48c496e397403d430f4b30
Helder De Oliveira <hemarque@gmail.com>
2022-06-02 15:39:16 +0200

    fizzbuzz of 3 is Fizz

--- challenge_0_fizzbuzz/java/fizzbuzz/src/test/java/FizzBuzzTest.java
+++ challenge_0_fizzbuzz/java/fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -5,16 +5,27 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void fizzbuzzOf1is1() {
-        assertEquals(1, fizzbuzzOf(1));
+        assertEquals("1", fizzbuzzOf(1));
     }
 
     @Test
     public void fizzbuzzOf2is2() {
-        assertEquals(2, fizzbuzzOf(2));
+        assertEquals("2", fizzbuzzOf(2));
     }
 
-    private int fizzbuzzOf(int number) {
-        return number;
+    @Test
+    public void fizzbuzzOf3isFizz() {
+        assertEquals("Fizz", fizzbuzzOf(3));
+    }
+
+    private String fizzbuzzOf(int number) {
+        String fizzbuzz;
+        if(number == 3){
+            fizzbuzz = "Fizz";
+        }else{
+            fizzbuzz = number + "";
+        }
+        return fizzbuzz;
     }
 
 }
