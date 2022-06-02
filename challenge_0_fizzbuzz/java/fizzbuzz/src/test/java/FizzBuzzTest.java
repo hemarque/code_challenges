0903e53be1fb9ce94f755f0ac89a04d3fbf5b7a4
Helder De Oliveira <hemarque@gmail.com>
2022-06-02 15:33:13 +0200

    fizzbuzz of 2 is 2

--- challenge_0_fizzbuzz/java/fizzbuzz/src/test/java/FizzBuzzTest.java
+++ challenge_0_fizzbuzz/java/fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -1,12 +1,20 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void testEnv(){
-        assertTrue(true);
+    public void fizzbuzzOf1is1() {
+        assertEquals(1, fizzbuzzOf(1));
+    }
 
+    @Test
+    public void fizzbuzzOf2is2() {
+        assertEquals(2, fizzbuzzOf(2));
+    }
+
+    private int fizzbuzzOf(int number) {
+        return number;
     }
 
 }
